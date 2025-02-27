dde927489ea1e60aac87ad06260c3a3ab25a0f25
LewisLosa <lewislosa@gmail.com>
2025-02-28 01:41:09 +0300

    Added: ViewModels with class files.

---
+++ Duru.Core/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duru.Core.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalRooms { get; set; }
+        public int AvailableRooms { get; set; }
+        public int OccupiedRooms { get; set; }
+        public int MaintenanceRooms { get; set; }
+        public int ActiveReservations { get; set; }
+        public int TodayCheckIns { get; set; }
+        public int TodayCheckOuts { get; set; }
+        public double TodayRevenue { get; set; }
+        public List<ReservationViewModel>? RecentReservations { get; set; }
+    }
+}
---
+++ Duru.Core/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duru.Core.ViewModels
+{
+    public class ReservationViewModel
+    {
+        public int ReservationId { get; set; }
+
+        [Required(ErrorMessage = "Guest is required")]
+        public int GuestId { get; set; }
+
+        public string GuestName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Room is required")]
+        public int RoomId { get; set; }
+
+        public string RoomNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Check-in date is required")]
+        public DateTime CheckIn { get; set; }
+
+        [Required(ErrorMessage = "Check-out date is required")]
+        public DateTime CheckOut { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string Status { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public int StayDuration => (CheckOut - CheckIn).Days;
+
+        public List<ReservationServiceViewModel>? Services { get; set; }
+
+        public double TotalAmount { get; set; }
+    }
+}
